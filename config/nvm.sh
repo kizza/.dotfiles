a9aa24e31447e8dacf1bf1ca688d6f1296784abc
Keiran <keiran.oleary@everydayhero.com.au>
2016-10-10 10:19:41 +1000

    Auto run nvm on path change

--- config/nvm.sh
+++ config/nvm.sh
@@ -1,1 +1,13 @@
 source ~/.nvm/nvm.sh
+
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc  ]]; then
+    nvm use
+  elif [[ $(nvm version) != $(nvm version default) ]]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
