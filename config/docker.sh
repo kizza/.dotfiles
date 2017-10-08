89297c56cf1da27634ee4565430f6a2e0bc6543f
Keiran <keiran.oleary@everydayhero.com.au>
2017-10-09 09:47:53 +1000

    Remove dinghy config (for now)

--- config/docker.sh
+++ config/docker.sh
@@ -1,6 +1,6 @@
 export COMPOSE_HTTP_TIMEOUT=60000
 
 alias dc="docker-compose"
-alias dockerenv="eval \"$(docker-machine env default)\""
-alias dinghyenv="eval \"$(dinghy env)\""
-dinghyenv
+# alias dockerenv="eval \"$(docker-machine env default)\""
+# alias dinghyenv="eval \"$(dinghy env)\""
+# dinghyenv
