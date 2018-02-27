da38f6f87f6028863c6b1681f5ef68c12b157cc9
Keiran <keiran.oleary@everydayhero.com.au>
2018-02-27 10:33:37 +1000

    Current brew installs

---
+++ config/brew.sh
@@ -0,0 +1,24 @@
+# ansible			gmp			memcached		qt
+# apple-gcc42		gnutls			mono			rabbitmq
+# autoconf		gobject-introspection	msgpack			rbenv
+# automake		graphicsmagick		mysql			readline
+# awscli			harfbuzz		ncdu			redis
+# boost			htop			neovim			ruby
+# cairo			icu4c			nettle			ruby-build
+# chromedriver		imagemagick		nginx			ruby-install
+# cmake			jemalloc		node			sl
+# coreutils		jpeg			nuget			sqlite
+# ctags			jpeg-turbo		oniguruma		the_silver_searcher
+# direnv			jq			openssl			tmux
+# dnsmasq			libcroco		ossp-uuid		unfs3
+# elixir			libevent		pango			unibilium
+# erlang			libffi			parallel		unixodbc
+# fontconfig		libpng			pcre			utf8proc
+# freetype		librsvg			perl			vim
+# fswatch			libtasn1		phantomjs		wemux
+# fzf			libtermkey		pixman			wxmac
+# gdbm			libtiff			pkg-config		xz
+# gdk-pixbuf		libtool			postgresql		zsh
+# gettext			libuv			pow			zsh-completions
+# git-secrets		libvterm		pssh			zsh-syntax-highlighting
+# glib			libyaml			python
