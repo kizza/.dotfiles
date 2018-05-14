930f73e9364e3e94ca9e94d8ff0ed4b8c5e1f8bf
Keiran <hello@keiranoleary.com>
2018-05-15 09:33:53 +1000

    Update nvm config

--- config/nvm.sh
+++ config/nvm.sh
@@ -1,7 +1,6 @@
-source ~/.nvm/nvm.sh
-
 export NVM_DIR="$HOME/.nvm"
-   . "/usr/local/opt/nvm/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 autoload -U add-zsh-hook
 load-nvmrc() {
