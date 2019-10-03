e52960fb4856c38cdce968785d660cea8f538b0e
Keiran <hello@keiranoleary.com>
2019-10-03 15:52:36 +1000

    Use base16 with bat terminal preview

--- config/base-16.sh
+++ config/base-16.sh
@@ -2,3 +2,5 @@ BASE16_SHELL="$HOME/.config/base16-shell"
 [ -n "$PS1" ] && \
   [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
     eval "$(bash $BASE16_SHELL/profile_helper.sh)"
+
+export BAT_THEME="base16"
