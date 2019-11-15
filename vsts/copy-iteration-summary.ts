30e23edfa3c42a803d31ac925264d0268f4b4d1d
Keiran <hello@keiranoleary.com>
2019-11-15 11:55:48 +1000

    Typescript version of vsts iteration summary

--- vsts/copy-iteration-summary.ts
+++ vsts/copy-iteration-summary.ts
@@ -1,18 +1,72 @@
+type Url = string;
+
+type Relation = { id: string; url: Url };
+
+type Iteration = { name: string; path: string; url: Url };
+
+type WorkItem = {
+  source: Relation | null;
+  target: Relation;
+};
+
+type HydratedIteration = {
+  _links: {
+    workitems: { href: Url };
+  };
+};
+
+type PartitionedIteration = Record<WorkItemState, FormattedWorkItem[]>;
+
+type HydratedWorkItem = {
+  fields: {
+    "System.Title": string;
+    "System.State": WorkItemState;
+    "Microsoft.VSTS.Scheduling.StoryPoints": number;
+    "System.WorkItemType": string;
+    _links: {
+      html: { href: Url };
+    };
+  };
+};
+
+type FormattedWorkItem = {
+  title: string;
+  state: WorkItemState;
+  points: number;
+  type: string;
+  link: Url;
+};
+
+enum WorkItemState {
+  Closed,
+  New,
+  Active,
+  Resolved
+}
+
+type AsyncFunction<T> = (args: any) => Promise<T> | T;
+
+// var asyncMap = <T>(fun: AsyncFunction<T>) => (items: any[]) =>
+//   Promise.all(items.map(fun));
+
+var tap = <T>(fun: Function) => (input: T) => {
+  fun(input);
+  return input;
+};
+
 var { assign, keys } = Object;
 
 var organisation = "";
 
 var product = "";
 
-var fetchJson = url => fetch(url).then(response => response.json());
+var fetchJson = (url: Url) => fetch(url).then(response => response.json());
 
-var asyncMap = fun => items => Promise.all(items.map(fun));
-
-var copyToClipboard = message => {
+var copyToClipboard = (message: string) => {
   const el = document.createElement("textarea");
   el.value = message;
   el.setAttribute("readonly", "");
-  el.style = { position: "absolute", left: "-9999px" };
+  (el as any).style = { position: "absolute", left: "-9999px" };
   document.body.appendChild(el);
   el.select();
   document.execCommand("copy");
@@ -27,27 +81,32 @@ var getIterationNameFromUrl = () =>
     .pop()
     .replace(/%20/g, " ");
 
-var filterToStoryUrl = workItems =>
+var filterToStoryUrl = (workItems: WorkItem[]) =>
   workItems.filter(item => item.source === null).map(item => item.target.url);
 
-var getWorkItemsFromIteration = iteration =>
+var getWorkItemsFromIteration = (
+  iteration: HydratedIteration
+): Promise<WorkItem[]> =>
   Promise.resolve(iteration["_links"].workitems.href)
     .then(fetchJson)
     .then(json => json.workItemRelations);
 
-var getIterationsList = () =>
+var getIterationsList = (): Promise<Iteration[]> =>
   fetchJson(
     `https://${organisation}.visualstudio.com/Products/${product}/_apis/work/teamsettings/iterations?api-version=5.1`
   ).then(iterations => iterations.value);
 
-var partitionWorkItems = workItems =>
-  workItems.reduce((acc, each) => {
-    const key = each.state;
-    const existing = acc[key] || [];
-    return assign(acc, {
-      [key]: existing.concat(each)
-    });
-  }, {});
+var partitionWorkItems = (workItems: FormattedWorkItem[]) =>
+  workItems.reduce(
+    (acc, each) => {
+      const key = each.state;
+      const existing = acc[key] || [];
+      return assign(acc, {
+        [key]: existing.concat(each)
+      });
+    },
+    {} as PartitionedIteration
+  );
 
 var countPartitionPoints = partition =>
   partition.reduce((acc, each) => acc + each.points, 0);
@@ -62,7 +121,7 @@ var partitionAsBlurb = (state, partition) => {
   return `${stateToEmoji(state)} ${points}\n\n${items}`;
 };
 
-var stateToEmoji = state => {
+var stateToEmoji = (state: WorkItemState) => {
   const emoji = {
     Closed: ":balloon: *Closed*",
     New: ":sparkles: *New stories*",
@@ -86,7 +145,7 @@ var partitionSummary = partitionedWorkItems =>
     )
     .join("\n");
 
-var partitionSummaryWithItems = partitionWorkItems =>
+var partitionSummaryWithItems = (partitionWorkItems: PartitionedIteration[]) =>
   keys(partitionWorkItems)
     .reduce(
       (acc, key) => acc.concat(partitionAsBlurb(key, partitionWorkItems[key])),
@@ -94,7 +153,7 @@ var partitionSummaryWithItems = partitionWorkItems =>
     )
     .join("\n\n\n");
 
-var formatWorkItems = workItems =>
+var formatWorkItems = (workItems: HydratedWorkItem[]): FormattedWorkItem[] =>
   workItems.map(item => ({
     title: item.fields["System.Title"].trim(),
     state: item.fields["System.State"],
@@ -103,32 +162,35 @@ var formatWorkItems = workItems =>
     link: item["_links"].html.href
   }));
 
-var partitionIteration = iteration =>
+var partitionIteration = (iteration: HydratedIteration) =>
   getWorkItemsFromIteration(iteration)
     .then(filterToStoryUrl)
-    .then(asyncMap(fetchJson))
+    .then(asyncMap<HydratedWorkItem>(fetchJson))
     .then(formatWorkItems)
     .then(partitionWorkItems);
 
-var filterIteration = (partitionedIteration, predicate) =>
+var filterIteration = (
+  partitionedIteration: PartitionedIteration,
+  predicate: (key: string) => boolean
+) =>
   keys(partitionedIteration).reduce(
     (acc, key) =>
       predicate(key) ? assign(acc, { [key]: partitionedIteration[key] }) : acc,
-    {}
+    {} as PartitionedIteration
   );
 
 var closedPartitionedPredicate = key =>
   ["Closed", "Resolved"].indexOf(key) !== -1;
 
-var couldNotFindIterationWithin = (name, iterations) => {
+var couldNotFindIterationError = (name, iterations) => {
   alert(
     `Could not find this iteration "${name}", check console log for more details`
   );
   console.error(`Could not find iteration "${name}" within`, iterations);
-  return new Error(`Could not find iteration "${name}"`);
+  return Promise.reject(`Could not find iteration "${name}"`);
 };
 
-var stringEquals = (string1, string2) =>
+var stringEquals = (string1: string, string2: string) =>
   string1.toUpperCase() === string2.toUpperCase();
 
 var getCurrentAndPrevoiusIterations = () =>
@@ -139,7 +201,7 @@ var getCurrentAndPrevoiusIterations = () =>
     );
 
     if (!currentIteration) {
-      return couldNotFindIterationWithin(currentInterationName, iterations);
+      return couldNotFindIterationError(currentInterationName, iterations);
     }
 
     const previousIteration =
@@ -148,12 +210,8 @@ var getCurrentAndPrevoiusIterations = () =>
     return [currentIteration, previousIteration];
   });
 
-var hydrateIteration = iteration => fetchJson(iteration.url);
-
-var tap = fun => input => {
-  fun(input);
-  return input;
-};
+var hydrateIteration = (iteration: Iteration): Promise<HydratedIteration> =>
+  fetchJson(iteration.url);
 
 getCurrentAndPrevoiusIterations()
   .then(asyncMap(hydrateIteration))
@@ -163,7 +221,7 @@ getCurrentAndPrevoiusIterations()
     filterIteration(previous, closedPartitionedPredicate)
   ])
   .then(
-    tap(([current, previous]) =>
+    tap<PartitionedIteration[]>(([current, previous]) =>
       console.log("Previous", previous, "Current", current)
     )
   )
