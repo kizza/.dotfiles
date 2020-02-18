f5950b04e517e11af8775f7e638c881b73bf5912
Keiran <hello@keiranoleary.com>
2020-02-18 10:14:37 +1000

    Add docker aliases

--- config/docker.sh
+++ config/docker.sh
@@ -1,6 +1,10 @@
 export COMPOSE_HTTP_TIMEOUT=60000
 
 alias dc="docker-compose"
+alias stopcontainers="docker stop $(docker ps -a -q)"
+alias removecontainers="docker rm $(docker ps -a -f status=exited -q)"
+alias removeimages="docker rmi $(docker images -a -q)"
+
 # alias dockerenv="eval \"$(docker-machine env default)\""
 # alias dinghyenv="eval \"$(dinghy env)\""
 # dinghyenv
