8fcefc60b16c08bb0520bd24afbdf94ea35d9112
Keiran <hello@keiranoleary.com>
2020-06-01 10:13:19 +1000

    Lazy eval docker aliases

--- config/docker.sh
+++ config/docker.sh
@@ -1,9 +1,16 @@
 export COMPOSE_HTTP_TIMEOUT=60000
 
 alias dc="docker-compose"
-alias stopcontainers="docker stop $(docker ps -a -q)"
-alias removecontainers="docker rm $(docker ps -a -f status=exited -q)"
-alias removeimages="docker rmi $(docker images -a -q)"
+
+stopcontainers() {
+  docker stop $(docker ps -a -q)
+}
+removecontainers() {
+  docker rm $(docker ps -a -f status=exited -q)
+}
+removeimages() {
+  docker rmi $(docker images -a -q)
+}
 
 # alias dockerenv="eval \"$(docker-machine env default)\""
 # alias dinghyenv="eval \"$(dinghy env)\""
