0496a0744dd8dcc36496bca0d94250fa792e57b2
Keiran O'Leary <hello@keiranoleary.com>
2021-02-23 16:29:16 +1000

    Update open-git-commits favelet

--- misc/favelets/open-git-commits.ts
+++ misc/favelets/open-git-commits.ts
@@ -1,9 +1,7 @@
-[...document.querySelectorAll("a[href*='/commits/']")]
+Array.from(document.querySelectorAll("a[href*='/commits/']"))
   .map(link => link.attributes["href"].value)
-  .reduce((acc, href) => ({
-    ...acc,
-    [href]: true
-  }), {})
-  .map(hrefs => Object.keys(hrefs))
-  .forEach(href => console.log(href))
-// .forEach(href => window.open(href));
+  .reduce(
+    (acc, href) => (acc.indexOf(href) === -1 ? acc.concat([href]) : acc),
+    []
+  )
+  .forEach(href => window.open(href));
