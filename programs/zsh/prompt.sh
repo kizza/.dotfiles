621674f1ca037916d983f19b4304674337604159
Keiran O'Leary <hello@keiranoleary.com>
2021-03-25 09:58:40 +1000

    Update prompt (support better vim ui)

--- programs/zsh/prompt.sh
+++ programs/zsh/prompt.sh
@@ -1,5 +1,12 @@
-local ret_status="%(?:%{$fg_bold[green]%}:%{$fg_bold[red]%})"
+local icon=""
+local ret_status="%(?:%{$fg_bold[green]%}$icon:%{$fg_bold[red]%}$icon)"
 local background_job_status="%(1j:%{$fg[cyan]%} ...:)"
+local vim_prompt="%{$fg_bold[yellow]%}VIM MODE%{$reset_color%}"
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[blue]%} "
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%} %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}"
 
 nix_shell_status () {
   if [ ! -z $IN_NIX_SHELL ]; then
@@ -7,19 +14,19 @@ nix_shell_status () {
   fi
 }
 
-PROMPT=' $(nix_shell_status)${ret_status} %{$fg[cyan]%}%c%{$reset_color%}$(git_prompt_info)${background_job_status} '
-
-ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[blue]%} "
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%} %{$fg[yellow]%}✗"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}"
+set_prompt () {
+  if [ $KEYMAP = vicmd ]; then
+    PS1=""
+    RPS1="$vim_prompt $EPS1"
+  else
+    PS1=" $(nix_shell_status)${ret_status} %{$fg[cyan]%}%c%{$reset_color%}$(git_prompt_info)${background_job_status} "
+    RPS1="$EPS1"
+  fi
+}
 
 # Vim prompt
-
 function zle-line-init zle-keymap-select {
-  VIM_PROMPT="%{$fg_bold[yellow]%} [% INSERT MODE]%  %{$reset_color%}"
-  # RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/}$(git_custom_status) $EPS1"
-  RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+  set_prompt
   zle reset-prompt
 }
 
@@ -29,5 +36,4 @@ export KEYTIMEOUT=1
 
 
 # History substring search
-
 export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND=bg=18,fg=green,bol
