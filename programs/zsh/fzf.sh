9c794e942b1501e80fe4b55066cc10dea257bccc
Keiran O'Leary <hello@keiranoleary.com>
2022-08-14 19:32:39 +1000

    Update fzf prompt

--- programs/zsh/fzf.sh
+++ programs/zsh/fzf.sh
@@ -18,7 +18,8 @@ fi
 # prompt: FZF> bit
 # pointer: The left cursor
 export FZF_DEFAULT_OPTS='
+--inline-info
 --color=light
 --color=fg:8,bg:-1,hl:2,fg+:15,bg+:18,hl+:2
---color=info:14,prompt:6,pointer:2,marker:6,spinner:#61afef,header:17
+--color=info:14,prompt:5,pointer:2,marker:6,spinner:3,header:17
 '
