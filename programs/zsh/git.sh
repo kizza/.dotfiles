c59c3435e76aa66a334a99614fe984dc2d194cf4
Keiran O'Leary <hello@keiranoleary.com>
2022-08-25 16:26:42 +1000

    Tweak wip "skip ci" suffix

--- programs/zsh/git.sh
+++ programs/zsh/git.sh
@@ -206,7 +206,7 @@ wip() {
   else
     message="WIP"
   fi
-  git commit -m "$(printf "$message\n\n[ci skip]\n")"
+  git commit -m "$(printf "$message\n\n[skip ci]\n")"
 }
 
 alias unwip='git log -n 1 | grep -q -c "WIP" && git reset HEAD~1'
