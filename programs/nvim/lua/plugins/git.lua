8ea643f8d9e4a5b0fb51e9435b7a5bcdc5d9b0d4
Keiran O'Leary <hello@keiranoleary.com>
2023-09-08 22:42:35 +1000

    Use gitsign lines over icons

--- programs/nvim/lua/plugins/git.lua
+++ programs/nvim/lua/plugins/git.lua
@@ -36,10 +36,10 @@ return {
     },
     init = function()
       vim.g.gitgutter_realtime = 1
-      vim.g.gitgutter_sign_added = ''
-      vim.g.gitgutter_sign_modified = ''
-      vim.g.gitgutter_sign_removed = ''
-      vim.g.gitgutter_sign_modified_removed = '⇎'
+      vim.g.gitgutter_sign_added = '│'
+      vim.g.gitgutter_sign_modified = '│'
+      vim.g.gitgutter_sign_removed = '│'
+      vim.g.gitgutter_sign_modified_removed = '││'
     end,
   },
 }
