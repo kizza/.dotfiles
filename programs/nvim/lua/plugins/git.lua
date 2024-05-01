5b1ef2992cd9302c79ec1a7c7fd8ab4cb5afbc01
Keiran O'Leary <hello@keiranoleary.com>
2024-05-01 22:07:00 +1000

    Switch to gitsigns

--- programs/nvim/lua/plugins/git.lua
+++ programs/nvim/lua/plugins/git.lua
@@ -28,22 +28,59 @@ return {
     end
   },
   {
-    "airblade/vim-gitgutter",
-    -- enabled = false,
+    "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
-    keys = {
-      { "<leader>hp", "<cmd>GitGutterPreviewHunk<cr>", desc = "Preview hunk" },
-      { "<leader>hs", "<cmd>GitGutterStageHunk<cr>",   desc = "Stage hunk" },
-      { "<leader>hu", "<cmd>GitGutterUndoHunk<cr>",    desc = "Undo hunk" },
-      { "[h",         "<cmd>GitGutterPrevHunk<cr>",    desc = "Previous git hunk" },
-      { "]h",         "<cmd>GitGutterNextHunk<cr>",    desc = "Next git hunk" },
-    },
-    init = function()
-      vim.g.gitgutter_realtime = 1
-      vim.g.gitgutter_sign_added = '│'
-      vim.g.gitgutter_sign_modified = '│'
-      vim.g.gitgutter_sign_removed = '│'
-      vim.g.gitgutter_sign_modified_removed = '││'
-    end,
+    config = function()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']h', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', '[h', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          -- Actions
+          map('n', '<leader>hs', gs.stage_hunk)
+          map('n', '<leader>hr', gs.reset_hunk)
+          map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+          map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+
+          -- Highlights
+          local colours = require("colours")
+          local hi = colours.hi
+          hi("GitSignsChange", { link = "DiffChange" })
+          hi("GitSignsAddInline", { fg = colours.green, reverse = true })
+          hi("GitSignsDeleteInline", { fg = colours.red, reverse = true })
+          hi("GitSignsChangeInline", { fg = colours.magenta, reverse = true })
+        end
+      }
+    end
   },
 }
