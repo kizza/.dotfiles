f5d18a117247f9bd05bf191f09e6d636975be71c
Keiran O'Leary <hello@keiranoleary.com>
2024-12-27 14:23:10 +1000

    Fix tmux default terminal

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -8,7 +8,17 @@ in
     enable = true;
     baseIndex = 1;
     escapeTime = 0;
-    extraConfig = builtins.readFile ./tmux.conf;
+    extraConfig = let
+      shellPackage = pkgs.zsh;
+      defaultCommand = if pkgs.stdenv.isDarwin then
+        "exec ${pkgs.reattach-to-user-namespace}/bin/reattach-to-user-namespace -l ${shellPackage}/bin/zsh"
+      else
+        "exec ${shellPackage}/bin/zsh";
+    in ''
+      set-option -g default-command '${defaultCommand}'
+
+      ${builtins.readFile ./tmux.conf}
+    '';
     newSession = true;
     plugins = [];
     terminal = "screen-256color";
