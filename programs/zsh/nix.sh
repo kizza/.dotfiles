488736c3211233e06987fb5b3df00e144cf35963
Keiran O'Leary <hello@keiranoleary.com>
2025-01-01 12:19:30 +1000

    Avoid maxos nix update woes

--- programs/zsh/nix.sh
+++ programs/zsh/nix.sh
@@ -1,1 +1,6 @@
+# avoid macOS updates to destroy nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+
 [ -s ~/.nix-profile/etc/profile.d/nix.sh ] && \. ~/.nix-profile/etc/profile.d/nix.sh
