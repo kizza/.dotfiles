a92150265e218f00482ea1916028867c9360b35b
Keiran O'Leary <hello@keiranoleary.com>
2025-01-22 23:23:16 +1000

    Use statuscolumn for folding

--- programs/nvim/lua/plugins/ui/default.lua
+++ programs/nvim/lua/plugins/ui/default.lua
@@ -10,15 +10,17 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     event = "VeryLazy",
-    config = function(_, opts)
+    config = function(_, _opts)
       -- vim.o.foldcolumn = '1' -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
+      vim.o.statuscolumn =
+      "%s %=%{%v:lnum == line('.') ? v:lnum : v:relnum%} %{%foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? '' : '%#DiagnosticSignOk#') : ' ' %}"
 
       local handler = function(virtText, lnum, endLnum, width, truncate)
         local newVirtText = {}
-        local suffix = ('  %d '):format(endLnum - lnum)
+        local suffix = ('  %d lines'):format(endLnum - lnum)
         local sufWidth = vim.fn.strdisplaywidth(suffix)
         local targetWidth = width - sufWidth
         local curWidth = 0
