e6be45e78d73be778eaf72b36f1418d493eb771d
Keiran O'Leary <hello@keiranoleary.com>
2025-06-10 16:51:26 +1000

    Extract contextmenu as plugin

---
+++ programs/nvim/lua/plugins/contextmenu.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    -- "kizza/contextmenu.nvim",
+    dir = "~/Code/kizza/contextmenu.nvim",
+    enabled = true,
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      local contextmenu = require("contextmenu")
+      contextmenu.setup(opts)
+
+      -- Stylize with background theme
+      local theme = require("colours")
+      local icon_colour = 3
+      local background_colour = 18
+      theme.hi("CmenuNormal", { fg = background_colour, bg = icon_colour })
+
+      local theme_colour = 0
+      background_colour = theme.darken(theme_colour, 0.3, { to = "#000000" })
+      theme.hi("CmenuNormal", { fg = background_colour, bg = icon_colour })
+      theme.hi("CmenuVirtualText", { bold = true })
+      theme.hi("Cmenu", { bg = background_colour })
+      -- theme.hi("CmenuSel", { bg = theme.darken(theme_colour, 0.2, { towards = "#000000" }) })
+    end,
+  }
+}
