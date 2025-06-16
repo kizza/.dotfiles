c24b8fa84498e08c2af5e3ee1567d491dcec6f31
Keiran O'Leary <hello@keiranoleary.com>
2025-06-16 16:31:47 +1000

    Refactor lualine

--- programs/nvim/lua/plugins/ui/status.lua
+++ programs/nvim/lua/plugins/ui/status.lua
@@ -10,25 +10,29 @@ local M = {
 }
 
 function M.build_theme()
-  local theme    = require 'lualine.themes.auto'
-  -- local theme    = require 'lualine.themes.base16'
-  local c        = require("colours").get
+  local theme           = require 'lualine.themes.auto'
+  local colours         = require("colours")
+  local c               = colours.get
 
-  -- theme.normal   = { a = { fg = 0, bg = 12, gui = "bold" }, b = {}, c = {} }
-  theme.normal   = { a = { fg = c(0), bg = c(12), gui = "bold" }, b = {}, c = {} }
-  theme.insert   = { a = { fg = c(0), bg = c(10), gui = "bold" }, b = {}, c = {} }
-  theme.visual   = { a = { fg = c(0), bg = c(11), gui = "bold" }, b = {}, c = {} }
-  theme.command  = { a = { fg = c(2), bg = c(0), gui = "bold" }, b = {}, c = {} }
-  theme.replace  = { a = { fg = c(0), bg = c(13), gui = "bold" }, b = {}, c = {} }
-  theme.terminal = theme.insert
-  theme.inactive = theme.normal
+  -- Base theme
+  theme.normal          = { a = { fg = c(0), bg = c(12), gui = "bold" }, b = {}, c = {} }
+  theme.insert          = { a = { fg = c(0), bg = c(10), gui = "bold" }, b = {}, c = {} }
+  theme.visual          = { a = { fg = c(0), bg = c(11), gui = "bold" }, b = {}, c = {} }
+  theme.command         = { a = { fg = c(2), bg = c(0), gui = "bold" }, b = {}, c = {} }
+  theme.replace         = { a = { fg = c(0), bg = c(13), gui = "bold" }, b = {}, c = {} }
+  theme.terminal        = theme.insert
+  theme.inactive        = vim.deepcopy(theme.normal)
 
-  local modes    = { "normal", "insert", "visual", "command", "replace" }
+  -- Standardize selected sections
+  local modes           = { "normal", "insert", "visual", "command", "replace" }
+  local styled_inactive = colours.darken(5, 0.7, { to = '#666666' }).hex
   for _, mode in pairs(modes) do
-    theme[mode]["b"] = { fg = c(7), bg = c(19) }                 -- Branch
-    theme[mode]["c"] = { fg = c(7), bg = c(18), gui = "italic" } -- File
-    theme[mode]["x"] = { fg = c(20), bg = c(18), gui = "" }      -- Meta
+    theme[mode]["b"] = { fg = c(7), bg = colours.darken(5, 0.8).hex } -- Branch
+    theme[mode]["c"] = { fg = c(7), bg = c(18), gui = "italic" }      -- File
+    theme[mode]["x"] = { fg = styled_inactive, bg = c(18), gui = "" } -- Meta
   end
+
+  theme.inactive.c = { fg = styled_inactive, bg = c(18), gui = "italic" } -- File
 
   return theme
 end
@@ -42,60 +46,48 @@ function M.config(_, opts)
     'filename',
     path = 1,
     shorting_target = 40,
-    symbols = { modified = '', readonly = '', unnamed = '[No Name]', newfile = '[New]' }
+    symbols = { modified = '', readonly = '', unnamed = '[No name]', newfile = '[New]' }
   }
 
   local line_location_section = function()
     return vim.fn.line(".") .. "/" .. vim.fn.line("$")
   end
 
+  local empty = require('lualine.component'):extend()
+  function empty:draw(default_highlight)
+    self.status = ''
+    self.applied_separator = ''
+    self:apply_highlights(default_highlight)
+    self:apply_section_separators()
+    return self.status
+  end
+
+  local colours = require("colours")
+
   require("lualine").setup {
     options = {
       theme = M.build_theme(),
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '╲', right = '╱' },
       disabled_filetypes = {
         statusline = { "fzf", "minimap" },
       },
-      extensions = { "nvim-tree" },
     },
     sections = {
       lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diagnostics' }, -- 'diff',
-      lualine_c = { filename_section },
-      lualine_x = { { 'filetype', colored = true } },
-      lualine_y = {
-        -- {
-        --   'lsp_progress',
-        --   display_components = { 'lsp_client_name', { 'percentage' }},
-        --   colors = {
-        --     percentage  = 20,
-        --     title  = "cyan",
-        --     message  = "cyan",
-        --     spinner = "cyan",
-        --     lsp_client_name = 20,
-        --     use = true,
-        --   },
-        --   separators = {
-        --     component = ' ',
-        --     progress = ' | ',
-        --     message = { pre = '(', post = ')'},
-        --     percentage = { pre = '', post = '%% ' },
-        --     title = { pre = '', post = ': ' },
-        --     lsp_client_name = { pre = '', post = '' },
-        --     spinner = { pre = '', post = '' },
-        --     message = { commenced = 'In Progress', completed = 'Completed' },
-        --   },
-        --   timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
-        --   spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' },
-        -- },
-        { 'progress' }
+      lualine_b = {
+        { 'branch', icon = { '', color = { fg = colours.get(4) } } }
       },
+      lualine_c = { filename_section },
+      lualine_x = { 'diagnostics', { 'filetype', colored = true } },
+      lualine_y = { { 'progress' } },
       lualine_z = { line_location_section }
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
       lualine_c = { filename_section },
-      lualine_x = { line_location_section },
+      lualine_x = { { 'filetype', colored = false }, line_location_section },
       lualine_y = {},
       lualine_z = {}
     },
