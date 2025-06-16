00eae157d06ddcd0406423701ebb32ea7781b613
Keiran O'Leary <hello@keiranoleary.com>
2025-06-17 00:00:55 +1000

    Add andriod dev env

--- programs/zsh/env.sh
+++ programs/zsh/env.sh
@@ -1,2 +1,7 @@
 export EDITOR=vim
 export DISABLE_SPRING=true
+
+# Android
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/emulator:$PATH"
