94fb6d3adf0591ded549e81d33ea24ad033e88ca
Keiran O'Leary <hello@keiranoleary.com>
2025-07-12 16:38:45 +1000

    Enable noice documentation polish

--- programs/nvim/lua/plugins/ui/noice.lua
+++ programs/nvim/lua/plugins/ui/noice.lua
@@ -16,6 +16,9 @@ return {
         progress = {
           enabled = false,
         },
+        documentation = {
+          enabled = true,
+        },
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
