f888c9336449a53010f9d987bdab9d6d239a9bd8
Keiran O'Leary <hello@keiranoleary.com>
2025-10-24 22:57:30 +1000

    Update lualine

--- programs/nvim/lua/plugins/ui/status.lua
+++ programs/nvim/lua/plugins/ui/status.lua
@@ -41,55 +41,134 @@ function M.refresh()
   require("lualine.highlight").create_highlight_groups(M.build_theme())
 end
 
-function M.build_spinner(opts)
-  local defaults = {
-    -- label = " Asking...",
-    symbols = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
-  }
-  opts = vim.tbl_extend("force", defaults, opts or {})
-  opts.symbol_size = #opts.symbols
-
+function M.build_spinner(fn)
+  -- local symbols = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+  local lualine = require('lualine')
+  local symbols = { "", "", "", "", "", "" }
   local C = require("lualine.component"):extend()
   C.processing = false
   C.label = nil
   C.spinner_index = 1
 
-  -- Initializer
+  -- Initialise lualine component and invoke setup fn
   function C:init(options)
     C.super.init(self, options)
+    fn(self)
+  end
 
-    local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+  -- Force a manual refresh
+  function C:refresh()
+    lualine.refresh()
+  end
 
+  -- Function that runs every time statusline is updated
+  local symbols_size = #symbols
+  function C:update_status()
+    local status = {}
+
+    if self.processing then
+      self.spinner_index = (self.spinner_index % symbols_size) + 1
+      table.insert(status, symbols[self.spinner_index])
+    end
+
+    if self.label then
+      table.insert(status, self.label)
+    end
+
+    return table.concat(status, " ")
+  end
+
+  return C
+end
+
+function M.build_ai_spinner()
+  return M.build_spinner(function(spinner)
+    local group = vim.api.nvim_create_augroup("CodeCompanionHooks", { clear = true })
     vim.api.nvim_create_autocmd({ "User" }, {
       -- pattern = "CodeCompanionRequest*",
       pattern = "CodeCompanion*",
       group = group,
       callback = function(request)
         if request.match == "CodeCompanionChatSubmitted" or request.match == "CodeCompanionRequestStarted" then
-          self.processing = true
-          self.label = " Asking"
+          spinner.processing = true
+          spinner.label = "Asking"
+          spinner:refresh()
         elseif request.match == "CodeCompanionChatStopped" or request.match == "CodeCompanionRequestFinished" then
-          self.processing = false
-          self.label = nil
+          spinner.processing = false
+          spinner.label = nil
         end
       end,
     })
-  end
-
-  -- Function that runs every time statusline is updated
-  function C:update_status()
-    if self.label then
-      self.spinner_index = (self.spinner_index % opts.symbol_size) + 1
-      return opts.symbols[self.spinner_index] .. self.label
-    else
-      return nil
-    end
-  end
-
-  return C
+  end)
 end
 
-function M.config(_, opts)
+function M.build_lsp_spinner()
+  return M.build_spinner(function(spinner)
+    local group = vim.api.nvim_create_augroup("LspHooks", {})
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = group,
+      callback = function(args)
+        -- print("LspAttach " .. vim.inspect(args))
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client then
+          spinner.processing = false
+          spinner.label = client.name
+          spinner:refresh()
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('LspProgress', {
+      group = group,
+      callback = function(args)
+        -- print("LspProgress " .. vim.inspect(args))
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local kind = args.data.params.value.kind
+        if client then
+          if kind == "begin" then
+            spinner.label = client.name
+            spinner.processing = true
+          elseif kind == "report" then
+            spinner.processing = true
+            -- spinner.label = args.data.params.value.title .. " " .. tostring(args.data.params.value.percentage) .. "%%"
+            spinner.label = client.name .. " " .. tostring(args.data.params.value.percentage) .. "%%"
+            -- spinner:refresh()
+          elseif kind == "end" then
+            spinner.processing = false
+            spinner.label = client.name
+          else
+            print("Unknown lsp progress " .. vim.inspect(args))
+          end
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('LspRequest', {
+      group = group,
+      callback = function(args)
+        -- print("LspRequest " .. vim.inspect(args))
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local method = args.data.request.method
+        local type = args.data.request.type
+        if client then
+          if type == "pending" then
+            spinner.label = client.name .. " " .. method
+            spinner.processing = true
+          elseif type == "complete" then
+            spinner.processing = false
+            spinner.label = client.name
+          elseif type == "cancel" then
+            --
+          else
+            print("Unknown lsp request " .. vim.inspect(args))
+          end
+        end
+      end,
+    })
+  end)
+end
+
+local function build()
   local filename_section = {
     'filename',
     path = 1,
@@ -101,14 +180,14 @@ function M.config(_, opts)
     return vim.fn.line(".") .. "/" .. vim.fn.line("$")
   end
 
-  local empty = require('lualine.component'):extend()
-  function empty:draw(default_highlight)
-    self.status = ''
-    self.applied_separator = ''
-    self:apply_highlights(default_highlight)
-    self:apply_section_separators()
-    return self.status
-  end
+  -- local empty = require('lualine.component'):extend()
+  -- function empty:draw(default_highlight)
+  --   self.status = ''
+  --   self.applied_separator = ''
+  --   self:apply_highlights(default_highlight)
+  --   self:apply_section_separators()
+  --   return self.status
+  -- end
 
   local colours = require("colours")
 
@@ -127,9 +206,9 @@ function M.config(_, opts)
         { 'branch', icon = { '', color = { fg = colours.get(4) } } }
       },
       lualine_c = { filename_section },
-      lualine_x = { 'diagnostics', { 'filetype', colored = true } },
+      lualine_x = { M.build_lsp_spinner(), 'diagnostics', { 'filetype', colored = true } },
       lualine_y = {
-        { M.build_spinner() },
+        { M.build_ai_spinner() },
         { 'progress' }
       },
       lualine_z = { line_location_section }
