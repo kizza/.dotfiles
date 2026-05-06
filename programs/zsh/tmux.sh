5257715d330b86f3dc38330e7ea4fc646ba8c66a
Keiran O'Leary <hello@keiranoleary.com>
2026-05-07 09:40:02 +1000

    Skip tmux smart start

--- programs/zsh/tmux.sh
+++ programs/zsh/tmux.sh
@@ -1,9 +1,12 @@
-if command -v tmux &> /dev/null; then
-  if [[ -n "$TMUX" ]]; then
-    echo "Within tmux"
-    # tmux attach
-  else
-    echo "Creating tmux"
-    tmux new -s TMUX
-  fi
-fi
+# if command -v tmux &> /dev/null; then
+#   if [[ -n "$TMUX" ]]; then
+#     echo "Within tmux"
+#     # tmux attach
+#   else
+#     echo "Creating tmux"
+#     # tmux new -s TMUX
+
+#     # Start (or attach to existing) session
+#     tmux new-session -A -s TMUX
+#   fi
+# fi
