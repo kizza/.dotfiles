6fb9538f47481acf775e48a5210574f653029a64
Keiran O'Leary <hello@keiranoleary.com>
2026-07-01 14:32:04 +1000

    Add basecamp-cli

---
+++ programs/basecamp-cli.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+let
+  version = "0.7.2";
+
+  platform =
+    if pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64 then "darwin_arm64"
+    else if pkgs.stdenv.isDarwin then "darwin_amd64"
+    else if pkgs.stdenv.isLinux && pkgs.stdenv.isAarch64 then "linux_arm64"
+    else "linux_amd64";
+
+  basecamp-cli = pkgs.stdenv.mkDerivation {
+    pname = "basecamp-cli";
+    inherit version;
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/basecamp/basecamp-cli/releases/download/v${version}/basecamp_${version}_${platform}.tar.gz";
+      hash = "sha256-9OL+wk1iAKTwMnhkH+3R6WCKWWZ5RtbB/xpZ3oa9sYI=";
+    };
+
+    sourceRoot = ".";
+
+    installPhase = ''
+      mkdir -p $out/bin
+      install -m755 basecamp $out/bin/basecamp
+    '';
+  };
+in
+{
+  home.packages = [
+    basecamp-cli
+  ];
+}
